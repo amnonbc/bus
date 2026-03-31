8a09a38813d450ed459a6d1ec468c43aeffb3835
amnonbc <amnonbc@gmail.com>
2026-03-31 06:49:04 +0100

    dev mode, open browser preview on startup

--- display_other.go
+++ display_other.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"log/slog"
+	"os/exec"
 	"sync/atomic"
 )
 
@@ -23,6 +24,10 @@ func runDisplay(active *atomic.Pointer[timeTable], weather *atomic.Pointer[strin
 	newHTTPPreview(buf, flip).register()
 	slog.Info("preview server", "url", "http://localhost:8080")
 	go listenHTTP()
+	err = exec.Command("open", "http://localhost:8080").Start()
+	if err != nil {
+		slog.Warn("could not open browser", "err", err)
+	}
 	buf.runLoop(active, weather, notify)
 	return nil
 }
