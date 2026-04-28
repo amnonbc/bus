f7ac5479c97bc2ace39585181c7505cda362054c
amnonbc <amnonbc@gmail.com>
2026-04-28 07:24:28 +0100

    use pidisp.Open, rather than inlining logic for selecting drm vs fb

--- display_linux.go
+++ display_linux.go
@@ -9,8 +9,7 @@ import (
 	"strings"
 	"sync/atomic"
 
-	"github.com/amnonbc/pidisp/drm"
-	"github.com/amnonbc/pidisp/fb"
+	"github.com/amnonbc/pidisp"
 )
 
 func logBoardModel() {
@@ -24,32 +23,15 @@ func logBoardModel() {
 func runDisplay(active *atomic.Pointer[timeTable], weather *atomic.Pointer[string], rotate bool, debug bool, forceFB bool, invert bool, notify <-chan struct{}, flip func()) error {
 	logBoardModel()
 
-	var hw blitter
-
-	if debug {
-		for _, card := range []string{"/dev/dri/card0", "/dev/dri/card1", "/dev/dri/card2"} {
-			drm.LogPlaneFormats(card)
-		}
+	hw, err := pidisp.Open(pidisp.Options{
+		Rotate:  rotate,
+		ForceFB: forceFB,
+		Debug:   debug,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to open display: %w", err)
 	}
-
-	if !forceFB {
-		d, err := drm.Open("/dev/dri/card0", rotate)
-		if err == nil {
-			defer d.Close()
-			hw = d
-		} else {
-			slog.Info("DRM unavailable, falling back to framebuffer", "err", err)
-		}
-	}
-
-	if hw == nil {
-		f, err := fb.Open("/dev/fb0", rotate)
-		if err != nil {
-			return fmt.Errorf("framebuffer: %w", err)
-		}
-		defer f.Close()
-		hw = f
-	}
+	defer hw.Close()
 
 	buf, err := newFrameBuffer(hw.Width(), hw.Height(), hw, invert)
 	if err != nil {
